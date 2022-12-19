5c7a8de328850da7c493ef0957ec2848e680a431
55973 <55973@etu.he2b.be>
2022-12-19 11:05:03 +0100

    exo2

--- public/main.js
+++ public/main.js
@@ -4,6 +4,10 @@ let b = 2
 let c=a+b
 console.log(c)
 
-// exo2
+// exo1
 let string = "J’aime les tartines et j’en mange"
 console.log(string)
+
+// exo2
+let exo2 = "4 par jours"
+console.log(exo2)
